0cf562bb385bfeee785ed116f8c49bfa3fb59eb8
Saumil Nalin <126989910+Saumiln@users.noreply.github.com>
2023-09-01 10:11:20 -0500

    Update Test1.java
    Fixed some compatibility issues

--- Test_project/Test1.java
+++ Test_project/Test1.java
@@ -1,9 +1,14 @@
 class Test1{
    private int uno;
    private int dos;
-   private int tres;
+   private int tres = 0;
    
-   public Test1(int one, int two, int tres) {
+   public Test1(int one, int two) {
+      uno = one;
+      dos = two;
+   }
+   
+   public Test1(int one, int two, int three) {
       uno = one;
       dos = two;
       tres = three;
