05f6bae95d58a7c1b0065254b49b37f8a89275e5
AaronK99 <aaronkurian14@gmail.com>
2023-08-31 22:42:11 -0500

    Update Test1.java
    Added a third variable named tres.

--- Test_project/Test1.java
+++ Test_project/Test1.java
@@ -1,13 +1,15 @@
 class Test1{
    private int uno;
    private int dos;
+   private int tres;
    
-   public Test1(int one, int two) {
+   public Test1(int one, int two, int tres) {
       uno = one;
       dos = two;
+      tres = three;
    }
    
    public void add(){
-      System.out.println(uno + dos);
+      System.out.println(uno + dos + tres);
    }
 }
